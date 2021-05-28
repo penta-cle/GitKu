2daa57c85e007e84079747232ff51a211664435f
pentacle <15538129007@163.com>
2021-05-28 11:46:04 +0800

    Eureka 搭建、学习

---
+++ springcloud/springcloud-eureka-7001/src/main/java/com/pentacle/springcloud/EurekaServe_7001.java
@@ -0,0 +1,13 @@
+package com.pentacle.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //EnableEurekaServer 服务器的启动类，可以接受别人注册进来
+public class EurekaServe_7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServe_7001.class, args);
+    }
+}
